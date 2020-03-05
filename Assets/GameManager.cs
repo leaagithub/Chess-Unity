880e543d4f624efe0bacabd0d284c8a99fa26e06
Aaron Le <ale2pwn@gmail.com>
2020-03-04 18:45:13 -0800

    Resets Board

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -22,6 +22,32 @@ public class GameManager : MonoBehaviour
             AllSquares[t.indRow, t.indCol] = t;
             EmptySquares.Add(t);
         }
+
+        AllSquares[0, 0].PieceTextureNumber = 12;
+        AllSquares[0, 1].PieceTextureNumber = 9;
+        AllSquares[0, 2].PieceTextureNumber = 7;
+        AllSquares[0, 3].PieceTextureNumber = 11;
+        AllSquares[0, 4].PieceTextureNumber = 8;
+        AllSquares[0, 5].PieceTextureNumber = 7;
+        AllSquares[0, 6].PieceTextureNumber = 9;
+        AllSquares[0, 7].PieceTextureNumber = 12;
+
+        AllSquares[7, 0].PieceTextureNumber = 6;
+        AllSquares[7, 1].PieceTextureNumber = 3;
+        AllSquares[7, 2].PieceTextureNumber = 1;
+        AllSquares[7, 3].PieceTextureNumber = 5;
+        AllSquares[7, 4].PieceTextureNumber = 2;
+        AllSquares[7, 5].PieceTextureNumber = 1;
+        AllSquares[7, 6].PieceTextureNumber = 3;
+        AllSquares[7, 7].PieceTextureNumber = 6;
+
+        for (int i = 0; i <8; ++i)
+        {
+            AllSquares[1, i].PieceTextureNumber = 10;
+            AllSquares[6, i].PieceTextureNumber = 4;
+        }
+
+
     }
 
     void Update()
